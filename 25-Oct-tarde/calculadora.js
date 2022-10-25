c4f494c065a2c5668e2139d6aa64c8861d771a9f
Irene <ireenelolo97@gmail.com>
2022-10-25 17:03:23 +0200

    Calculadora JS

--- 25-Oct-tarde/calculadora.js
+++ 25-Oct-tarde/calculadora.js
@@ -1,47 +1,36 @@
 // Calculadora
+let calculadora = document.getElementById("calculadora");
+let sumaEl = document.getElementById("suma");
+let restaEl = document.getElementById("resta");
+let productoEl = document.getElementById("producto");
+let divisionEl = document.getElementById("division");
 
-let calculadora = document.getElementById("calculadora")
-let suma = document.getElementById("suma")
-let resta = document.getElementById("resta")
-let producto = document.getElementById("producto")
-let division = document.getElementById("division")
+calculadora.innerText += "Resultados:"
 
-calculadora.innerText += "Resultados: \n"
+sumaEl.innerText += String(suma(10, 20));
+restaEl.innerText += String(resta(50, 10));
+productoEl.innerText += String(producto(11, 12));
+divisionEl.innerText += String(division(100, 50));
 
+// + - * /
 
-
-calculadora.innerText += suma (10, 20)
-calculadora.innerText += "\n"
-calculadora.innerText += resta(50, 10)
-calculadora.innerText += "\n"
-calculadora.innerText += producto (11, 11)
-calculadora.innerText += "\n"
-calculadora.innerText += division (100, 50)
-
-
-// + - * / 
-
-function suma (a, b){
+function suma(a, b){
     // Se espera que a y b sean números
-    let resultado = a + b
-    return resultado
+    let resultado = a + b;
+    return resultado;
 }
-function resta (a, b){
-    
-    let resultado = a - b
-    return resultado
+function resta(a, b){
+    let resultado = a - b;
+    return resultado;
 }
-function producto (a, b){
-    
-    let resultado = a * b
-    return resultado
+function producto(a, b){
+    let resultado = a * b;
+    return resultado;
 }
-function division (a, b){
+function division(a, b){
     // No se puede dividir por cero -> considerar ese caso
-    let resultado = a / b
-    return resultado
+    let resultado = a / b;
+    return resultado;
 }
 
 // Ejercicio -> colocamos cada resultado en su caja (div)
-
-
