3c706fd6073a747df384a420094f79118a13a0aa
Irene <ireenelolo97@gmail.com>
2022-10-27 15:45:11 +0200

    Acabamos el ejercicio y añadimos los diagramas explicativos

---
+++ 27-Oct/conversor-fraccion-a-decimal.md
@@ -0,0 +1,11 @@
+# Conversor de fracciones a decimal
+
+Calcula la representación en base 10 de una fracción. Excluye la división por cero. Permite elegir el número de decimales hasta `12`.
+
+![Diagrama](diagrama-fraccion-a-decimal.png)
+
+## Trabajo Futuro
+
+- Considerar el conversor inverso de decimal a fracción.
+- Considerar más tipos de números (decimal, fracción, porcentajes).
+- Enlazar herramientas entre sí.
---
+++ 27-Oct/fraccion-a-decimal.js
@@ -0,0 +1,27 @@
+/* Script para convertir fracciones en números decimales */
+
+function convertir(){
+    let numerador = document.getElementById("myInputA").valueAsNumber
+    let denominador = document.getElementById("myInputB").valueAsNumber
+    
+    /* Excluimos el caso de dividir por cero */
+    if( denominador === 0 ){
+        alert("No se puede dividir por cero.\n¡Acaba mal!" )
+        return ""
+    }
+
+    let decimales = document.getElementById("decimales").options.selectedIndex
+    let resultado = numerador / denominador
+
+    document.getElementById("resultados").innerText = String(resultado.toFixed(decimales))    //String opcional
+}
+
+function borrar(){
+    let myInputA = document.getElementById("myInputA")
+    let myInputB = document.getElementById("myInputB")
+    let resultado = document.getElementById("resultados")
+
+    myInputA.value = ""
+    myInputB.value = ""
+    resultado.innerText = ""
+}
