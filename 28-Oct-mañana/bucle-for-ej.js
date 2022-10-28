bab1c6241caf79ca92fb8c5e621cebb271d4509e
Irene <ireenelolo97@gmail.com>
2022-10-28 12:12:43 +0200

    formas con chars

--- 28-Oct-mañana/bucle-for-ej.js
+++ 28-Oct-mañana/bucle-for-ej.js
@@ -1,57 +1,62 @@
 /* Script para resolver ejercicios de bucle for con JS */
 
 //ej1()     // 1. Cuenta hacia adelante -> introducimos string.slice(0,-2)
-//ej2()     // 2. Cuenta atrás 
-//ej3()     // 3. Números pares con salto de línea 
+//ej2()     // 2. Cuenta atrás
+//ej3()     // 3. Números pares con salto de línea
 //ej4()     // 4. Tabla de multiplicar
+//ej5()     // 5. Comprueba si el resto de la división es 2
+//ej6()     // 6. Repite char N veces
+//ej7()     // 7. Contador base 5
+// ej8()    // 8. Triángulo izq
 
 // 1
 /*
-Usando un bucle for escribir un script que pida valor entero y 
+Usando un bucle for escribir un script que pida un valor entero y 
 muestre en pantalla una lista de números desde el 0 al valor tecleado. 
-Los números se seprarán por comas. 
+Los números se separarán por comas. 
 
-Salida: 0, 1, 2, 3, 4, 5
+Salida:
+0, 1, 2, 3, 4, 5  
 */
 
 function ej1(){
-let number = number(prompt("Introduce un número entero positivo:"))     // Si no recibimos número es NaN 
+    let number = Number(prompt("Introduce un número entero positivo:")) // Si no recibimos número es NaN
+    
+    if( isNaN(number) ){ alert("No podemos realizar la iteración") }
+    else if ( number < 1 ){ alert("Necesitamos un número positivo") }
 
-if( isNaN(number) ){alert("No podemos realizar la interacción") }
-else if ( number < 1 ){ alert("Necesitamos un número positivo") }
+    number = Math.floor(number) // 2.5 -> 2 redondeo a la baja
 
-number = Math.floor(number) // 2.5 -> 2 redondeo a la baja 
+    // Ejercicio 1
+    let texto = "";
+    for(let i = 0; i < number; i++){
+        texto += i + ", "
+    }
+    //
 
-// Ejercicio 1 
-let texto = ""
-for(let i = 0; i < number; i++ ){
-    texto += i + ","
-}
-//
-
-// quitar coma -> usamos slice (0,-2) para quitar los 2 últimos caracteres
-escribirSalida(texto.slice(0,-2),1)
+    // quitar coma -> usamos slice(0,-2) para quitar los 2 últimos caracteres
+    escribirSalida(texto.slice(0,-2), 1);
 }
 
 // 2 
 /*
-Se trata de que dado un número entero el script muestre una cuenta atrás,
+Se trata de que dado un número entero el script muesre una cuenta atrás, 
 es decir, una lista de numeros separados por coma desde el número tecleado hasta el 0.
 
-Salida: 
-5, 4, 3, 2, 1
+Salida:
+5,4,3,2,1
 */
 
 function ej2(){
-    let number = Number(prompt("Introduce un número entero positivo"))  // Si no recibimos número es NaN
+    let number = Number(prompt("Introduce un número entero positivo:")) // Si no recibimos número es NaN
 
-    if( isNaN(number)){alert("No podemos realizar la iteración")}
-    else if ( number < 1 ){ alert("Necesitamos un número positivo" ) }
+    if( isNaN(number) ){ alert("No podemos realizar la iteración") }
+    else if ( number < 1 ){ alert("Necesitamos un número positivo") }
 
-    number = Math.floor(number) // 2.5 -> 2 redondeo a la baja 
+    number = Math.floor(number) // 2.5 -> 2 redondeo a la baja
 
-    //Ej 2 
-    let texto=""
+    // Ej 2
+    let texto = "";
     for(let i = number; i > 0; i--){
         texto += i + ", "
     }
@@ -60,12 +65,13 @@ function ej2(){
     escribirSalida(texto.slice(0,-2), 2)
 }
 
-// 3
+// 3 
 /*
-Se quiere un bucle que genere una lista con todos los números pares positivos por debajo
-del número tecleado por el usuatio. Los números se aparecerán en una ventana alert con cambios de línea (caracter '\n')
+Se quiere un bucle que genere una lista con todos los números pares positivos por debajo 
+del número tecleado por el usuario. Los números se aparecerán en una ventana alert con 
+cambios de linea (carácter '\n'). 
 
-Salida 
+Salida
 2
 4
 6
@@ -73,12 +79,12 @@ Salida
 */
 
 function ej3(){
-    let number = solicitarDato()
+    let number = solicitarDato();
     // Ej 3
-    let texto = ""
+    let texto = "";
     for(let i = 1; i < number; i++){
         if( i % 2 == 0){
-            texto += i + "\n"
+            texto += i + "\n";
         }
     }
     escribirSalida(texto, 3)
@@ -87,7 +93,7 @@ function ej3(){
 /*
 Este script debe escribir la tabla de multiplicar del número que el usuario teclee. 
 La tabla mostrará en diferentes líneas cada producto y el resultado en la forma a x b = ab.
-Recuerda que el salto de línea es el caracter '\n'
+Recuerda que el salto de linea es el carácter "\n"
 
 4 x 1 = 4
 
@@ -129,9 +135,67 @@ function ej5(){
  */
 
 function ej6(){
-    //
+    let number = solicitarDato(); // número
+    let char = prompt("Introduce un caracter:");
+    // let texto = char.repeat(number); // alternativa con repeat()
+    let texto = "";
+    for(let i = 0; i < number; i++){
+        texto += char;
+    }
+    escribirSalida(texto, 6); 
 }
 
+/**
+ * Se quiere tener un contador de dos dígitos en base 5. 
+ * El funcionamiento es simple: el dígito de la derecha irá aumentando y 
+ * cuando pase de 4 su valor se pondrá a 0 y el dígito de la izquierda se incrementa en 1. 
+ * El contador debe deternerse cuando alcancemos el valor 1:4
+ 
+0:0
+0:1
+0:2
+0:3
+0:4
+1:0
+1:1
+1:2
+1:3
+1:4 -> fin aquí
+...
+4:4
+ */
+
+function ej7(){
+    /**
+     * - Podemos iterar hasta 25 por la base 5
+     */
+    let texto = "";
+    let idx = 0;
+    for(let i = 0; i < 10; i++){
+        if( (i > 0) & (i % 5 == 0) ){
+            idx++;
+        }
+        texto += idx + ":" + (i%5) + "\n";
+    }
+    escribirSalida(texto, 7);
+}
+
+/**
+ * Se trata de dibujar un triángulo con asteriscos. 
+ * El usuario tecleará un valor entero, el programa escribirá con asteriscos tantas lineas como diga ese número. 
+ * Cada linea estará formada por una serie de astericos tan larga como diga el número de línea en el que está.
+
+Para separar una línea de la siguiente en console o en alert debes usar "\n". En este ejercicio usa console.log.
+ */
+function ej8(){
+    let number = solicitarDato();
+    let texto = "";
+    for(let i = 1; i <= number; i++){
+        texto += "*".repeat(i);
+        texto += "\n";
+    }
+    escribirSalida(texto, 8)
+}
 /* Funciones Auxiliares */
 function solicitarDato(){
     let number = Number(prompt("Introduce un número entero positivo:")) // Si no recibimos número es NaN
