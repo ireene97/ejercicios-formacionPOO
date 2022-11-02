6abc30f45a007d58328520ad9e5d3f9e931dce43
Irene <ireenelolo97@gmail.com>
2022-11-02 13:01:00 +0100

    Ejercicio 7 - Clases Articulo y Proveedor

--- 02-Nov/ej-objetos-con-clases.js
+++ 02-Nov/ej-objetos-con-clases.js
@@ -1,14 +1,14 @@
 /* Ejercicios con Objetos en JS */
 
-// ej1() -- Obj alumno y método que promedia notas 
-// ej2() -- Obj para calcular el precio neto de un producto
-ej3() // -- Obj para calcular la densidad de la piedra en kg/m3 y en gr/cc 
-// ej4() -- Obj que representa a una cuenta bancaria 
-// ej5() -- Obj ficha de entrenamiento 
-// ej6() -- Objs bus y conductor 
-// ej7() -- Objs Artículo y Proveedor
-// ej8() -- Objs Alumnos 
-// ej9() -- Objs Cliente y Factura 
+// ej1() // Obj alumno y método que promedia notas 
+// ej2() // Obj para calcular el precio neto de un producto
+// ej3() // Obj para calcular la densidad de la piedra en kg/m3 y en gr/cc 
+// ej4() // Obj que representa a una cuenta bancaria 
+// ej5() // Obj ficha de entrenamiento 
+// ej6()// Objs bus y conductor 
+ ej7() // Objs Artículo y Proveedor
+// ej8() // Objs Alumnos 
+// ej9() // Objs Cliente y Factura 
 
 // Ejercicio 1 - Obj para guardar notas de alumno y método que promedia notas
 // Propiedades: nombre, apellido, ingles, prog, html (asignaturas)
@@ -139,7 +139,31 @@ function ej3(){
 // Ejercicio 4- Objeto que emula una cuenta de banco con ingresar() y retirar ()
     // consultarSaldo() escribe el saldo actual 
 function ej4(){
-    let cuenta = {
+    class Cuenta{
+        constructor(balance = 0){
+            this.balance = balance
+        }
+        ingresar(cantidad = 0){
+            this.balance += cantidad       
+            console.log("Balance actual: " + this.balance)
+            return this.balance.toFixed(2) + " €"
+        }
+        retirar(cantidad){
+            if(cantidad > this.balance){
+                alert("No se puede retirar esa cantidad")
+                return 0
+            } else if( cantidad < 0 ){
+                alert("No se admiten cantidades negativas")
+                return 0 
+            }
+            this.balance -= cantidad         // limiatos los decimales a 2 
+            return cantidad.tofixed(2)
+        }
+        consultarBalance(){
+            return this.balance + " €"
+        }
+    }
+    /* let cuenta = {
         "balance": 100.00,         // €
         "ingresar": function(cantidad){
             this.balance += cantidad       
@@ -161,16 +185,55 @@ function ej4(){
             return this.balance + " €"
         }
     }
-    // escribirResultado("Retirar 50 €: ", "Balance retirado: " + cuenta.retirar(50))
+    */
+
+    /* Objetos cuenta de clase Cuenta*/
+    let cuenta = new Cuenta()
+     //escribirResultado("Retirar 200 €: ", cuenta.retirar(200))
     // escribirResultado("Retirar -1 €: ", cuenta.retirar(-1))
-    // escribirResultado("Retirar 200 €: ", cuenta.retirar(200))
-    escribirResultado("Ingresamos una cantidad con decimales €: ", cuenta.ingresar(10.12345));
+    cuenta.ingresar(120)
+    /*cuenta.retirar(1)
+    cuenta.retirar(3.50)
+    cuenta.retirar(100)
+    cuenta.ingresar(19.98)*/
+    escribirResultado("Balance actual: ", cuenta.consultarBalance())
+    //escribirResultado("Retirar 50 €: ", "Balance retirado: " + cuenta.retirar(50))
+    // escribirResultado("Ingresamos una cantidad con decimales €: ", cuenta.ingresar(10.12345));
 
 }
 
 // Ej 5 - Ficha de entrenamiento
 function ej5(){
-    let ficha = {
+    class Ficha{
+        constructor(nombre = "", 
+                    apellido = "", 
+                    sesiones = 0.0, 
+                    numSesiones = 0
+                    )
+            {
+            this.nombre = nombre
+            this.apellido = apellido
+            this.sesiones = sesiones
+            this.numSesiones = numSesiones
+        }
+        anotarSesion(kms){
+            if(kms <0 ){
+                let msj = "No se pueden registrar valores negativos"
+                alert(msj)
+                return msj
+            }
+            this.sesiones += kms    // Guardamos los km de todas las sesiones 
+            this.numSesiones += 1   // Incrementamos el contador de sesiones 
+            return this.sesiones + "km"
+        }
+        calcMedia(){
+            if (this.sesiones == 0 ){alert("Aún no has realizado una sesión!!"); 
+            return "No hay sesiones registradas"
+        }
+            return (this.sesiones / this.numSesiones).toFixed(2) + " km por sesión"
+        }
+    }
+    /* let ficha = {
         "nombre": "Pepe",
         "apellido": "Grande", 
         "sesiones": 0,          // float de km -> 1.56
@@ -194,18 +257,74 @@ function ej5(){
             return (this.sesiones / this.numSesiones).toFixed(2) + " km por sesión"
         }
     }
-    // escribirResultado("Anotar -1 km: ", ficha.anotarSesion(-1));    // Debe arrojar alert
-    // escribirResultado("Media con 0 sesiones: ", ficha.calcMedia())  // Debe arrojar alert
-    escribirResultado("Anotar +10 km: ", ficha.anotarSesion(10));   // Debe retornar '10 km'
+    */
+
+    /* Objeto ficha de la clase Ficha*/
+    let ficha = new Ficha()
+    // Añadimos sesiones -> 100 km y 3 sesiones -> 33.33 km / sesion 
     ficha.anotarSesion(20);
     ficha.anotarSesion(70);
-    escribirResultado("Calculamos media en ficha de " + ficha.nombre, ficha.calcMedia());     // Debe retornar '10 km por sesión'
+    ficha.anotarSesion(10);
+
+    // escribirResultado("Anotar -1 km: ", ficha.anotarSesion(-1));    // Debe arrojar alert
+    // escribirResultado("Media con 0 sesiones: ", ficha.calcMedia())  // Debe arrojar alert
+    // escribirResultado("Anotar +10 km: ", ficha.anotarSesion(10));   // Debe retornar '10 km'
+    // escribirResultado("Calculamos media en ficha de " + ficha.nombre, ficha.calcMedia());     // Debe retornar '10 km por sesión'
     
 }
 
 // Ej 6 - Bus y conductor, bajar y subir pasajeros 
 function ej6(){
-    let bus = {
+    class Conductor{
+        constructor(nombre = "Conductor por defecto", licencia = 1234){
+            this.nombre = nombre
+            this.licencia = licencia
+        }
+        toString(){
+            return "Conductor: " + this.nombre + ", con número de licencia" + this.licencia
+        }
+    }
+    class Bus{
+        constructor(
+                    linea = "",
+                    empresa = "",
+                    capacidad = 40,
+                    pasajeros = 0,
+                    conductor = new Conductor("Pepe", 9087)
+                    )
+        {
+            this.linea = linea
+            this.empresa = empresa
+            this.capacidad = capacidad
+            this.pasajeros = pasajeros
+            this.conductor = conductor
+        }
+        subir(pasajerosQueSuben){
+            if(pasajerosQueSuben >= this.capacidad){
+                this.pasajeros = this.capacidad         // 50-> 40
+                return this.pasajeros
+            } else if(this.pasajeros + pasajerosQueSuben >= this.capacidad){
+                this.pasajeros = this.capacidad         // 39 + 39 = 78 -> NO
+                return this.pasajeros
+            }
+                // 10 + 11 = 21 -> SI
+                this.pasajeros += pasajerosQueSuben
+                return this.pasajeros
+        }
+        bajar(pasajerosQueBajan){
+            if( pasajerosQueBajan >= this.capacidad){ 
+                this.pasajeros = 0 
+                return this.pasajeros}      // 100 -> 0
+            else if( pasajerosQueBajan > this.pasajeros) { 
+                this.pasajeros = 0      // Hay 21 y bajan 50 -> 0
+                return this.pasajeros
+            }      
+            this.pasajeros -= pasajerosQueBajan
+            return this.pasajeros
+        }
+
+    }
+    /* let bus = {
         "linea": "Línea 1",
         "empresa": "movement",
         "capacidad": 40, 
@@ -245,19 +364,23 @@ function ej6(){
             console.log(this.pasajeros)
             return this.pasajeros
         }
-    }
+    } */
+
+    /* Objeto de clase Bus con objeto Conductor en una propiedad*/
+    let bus = new Bus
+    // bus.capacidad = 100
     /* En consola podemos ver el valor de this.pasajeros antes de cada return */
-    escribirResultado("Suben 25 pasajeros: ", bus.subir(25)); // 25
-    escribirResultado("Suben 35 pasajeros: ", bus.subir(35)); // 25 + 35 -> capacidad -> 40
-    escribirResultado("Bajan 45 pasajeros: ", bus.bajar(45)); // 0
+    //escribirResultado("Suben 25 pasajeros: ", bus.subir(25)); // 25
+    //escribirResultado("Suben 35 pasajeros: ", bus.subir(35)); // 25 + 35 -> capacidad -> 40
+    //escribirResultado("Bajan 45 pasajeros: ", bus.bajar(45)); // 0
     
     // Comprobamos que el conductor está asignado
-    // escribirResultado("Nombre del conductor: ", bus.conductor.nombre);      // José
+    //escribirResultado("Nombre del conductor: ", bus.conductor.nombre);      // José
     // escribirResultado("Licencia del conductor: ", bus.conductor.licencia)   // 1234
 
-    // escribirResultado("Bajan 100", bus.bajar(100)); // 0
-    // escribirResultado("Suben 35", bus.subir(35))    // 35
-    // escribirResultado("Bajan 40", bus.bajar(40));   // 0
+     escribirResultado("Bajan 100", bus.bajar(100)); // 0
+     escribirResultado("Suben 35", bus.subir(35))    // 35
+     escribirResultado("Bajan 34", bus.bajar(40));   // 0
 
     // escribirResultado("Bajan 100", bus.bajar(100)); // 0
     // escribirResultado("Suben 35", bus.subir(200))   // 40
@@ -266,49 +389,101 @@ function ej6(){
 
 // Ej 7 - Artículo y proveedor 
 function ej7(){
-   // let articuloPlantilla = {
-    //    "nombreProd": "Televisor 4K",
-    //    "precio": 450.00,
-    //    "proveedor":{   
-    //        "nombreProv":"Sharp",
-    //        "email": "sharp@es.com",
-   //         "telefono": "(+34) 656 536 348"
-    //    },
-   //     "telefono": function(){
-    //        return {
-    //            "nombre": this.proveedor.nombreProv,
-    //            "telefono": this.proveedor.telefono
-    //        }
-    //    }
-    //}
-    let articulo1 = {
-        "nombreProd": "Monitor",
+    class Proveedor{
+        constructor(
+            nombre = "Tecnoshop",
+            telefono = "(+34) 123 456 789",
+            email = "tcn@tecno.com"
+            )
+        {
+            this.nombre = nombre;
+            this.telefono = telefono;
+            this.email = email;
+        }
+        datos(){
+            return "-> El teléfono del " + this.nombre + " es " + this.telefono + " con email: " + this.email;
+        }
+    }
+    class Articulo{
+        constructor(
+            nombre = "Artículo por defecto", 
+            precio = 0.99, 
+            proveedor = new Proveedor("Proveedor por defecto", "123456789", "email@mail.com")
+            )
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.proveedor = proveedor;
+        }
+        datos(){
+            return "Artículo: " + this.nombre + "\nCon precio: " + this.precio + " €.";
+        }
+        telefono(){
+            return this.proveedor;
+        }
+        datosProveedor(){
+            return this.proveedor.datos();
+        }
+    }
+    // let articuloPlantilla = {
+    //     "nombreArticulo": "Producto 123",
+    //     "precio": 100.00,
+    //     "proveedor": {
+    //         "nombreProveedor": "Proveedor 12345",
+    //         "email": "test@test.com",
+    //         "telefono": "(+34) 123 456 789"
+    //     },
+    //     "telefono": function(){
+    //         return {
+    //             "nombre": this.proveedor.nombreArticulo,
+    //             "telefono": this.proveedor.telefono
+    //         }
+    //     }
+    // }
+    
+    /*let articulo = {
+        "nombreArticulo": "Monitor",
         "precio": 200.00,
-        "proveedor":{   
-            "nombreProv":"TecnoShop",
+        "proveedor": {
+            "nombreProveedor": "Tecnoshop",
             "email": "tcn@tecno.com",
-            "telefono": "(+34) 656 536 348"
+            "telefono": "(+34) 123 456 789"
         },
         "telefono": function(){
-            console.log("Teléfono de " + this.proveedor.nombreProv + " es " + this.proveedor.telefono)
+            console.log("Teléfono de " + this.proveedor.nombreProveedor + " es " + this.proveedor.telefono);
             return {
-                "nombreProv": this.proveedor.nombreProv, 
+                "nombreProveedor": this.proveedor.nombreProveedor,
                 "telefonoProveedor": this.proveedor.telefono
             }
         },
-        "escribirTelefono":function(){
-            return "El teléfono del " + this.proveedor.nombreProv + " es " + this.proveedor.telefono
+        "datosProveedor": function(){
+            return "XYZ El teléfono del " + this.proveedor.nombreProveedor + " es " + this.proveedor.telefono;
         }
     }
-    // escribirResultado("Datos del Proveedor: ", JSON.stringify(articulo1.telefono()))
-    // escribirResultado("Datos del Proveedor: ", "Tlf de " + articulo1.telefono().nombreProv + " es " + articulo1.telefono().telefonoProveedor)
-    
-    // let nombre = articulo1.telefono().nombreProv
-    // let tlf = articulo1.telefono().telefonoProveedor
-    // escribirResultado("Datos del Proveedor: ", "Teléfono de " + nombre + " es " + tlf)
+    */
 
-    escribirResultado("String formateado en método: ", articulo1.escribirTelefono())
+    // Escribimos datos de un artículo por defecto
+    // escribirResultado("Artículo por defecto: ", new Articulo().datos())
 
+    // Escribimos un artículo por defecto con Proveedor personalizado (sobreescrito)
+    escribirResultado(
+        "Datos proveedor asignado (sobreescrito):", 
+        new Articulo(undefined, undefined, new Proveedor("Nuevo Proveedor", "098", "Email@test.com")).datosProveedor()
+    )
+
+    // Escribimos datos del Proveedor del artículo sobreescrito y Proveedor sobreescrito
+    /*
+    escribirResultado(
+        "Datos por defecto del proveedor asignado del artículo por defecto: ", 
+        new Articulo("nombre", 1, new Proveedor("pepito", "123", "mail")).datosProveedor()
+        )
+    */
+
+    // Escribimos datos de un artículo creado en la llamada de escribirResultado
+    // escribirResultado("String formateado en método: ", new Articulo("Mi Artículo #1121", 300).datos())
+
+    // Escribimos datos de un nuevo Proveedor
+    // escribirResultado("Datos del Proveedor: ", new Proveedor("Pepe", "1212", "email123").datos())
 }
 
 // Ej 8 - Alumnos
