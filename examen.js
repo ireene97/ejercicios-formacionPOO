41a92026c944d163f55bc4aeb37c57499227bdfe
Irene <ireenelolo97@gmail.com>
2022-11-12 00:08:41 +0100

    Repaso

---
+++ examen.js
@@ -0,0 +1,22 @@
+// let a = 2
+// let b = 6
+
+
+// let suma = document.getElementById("suma")
+// let text1 = suma.innerText
+// text1 = text1 + (a + b)
+// suma.innerText = text1
+
+// let resta = document.getElementById("resta")
+// let text2 = resta.innerText
+// text2 = text2 + (a - b)
+// resta.innerText = text2
+
+
+
+function suma (){
+    let resultado = a + b
+    return resultado
+}
+
+const fun = (a, b) => { return a + b }
